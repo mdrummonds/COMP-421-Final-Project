e7f952087cd97b647c280c88164122b7721292f6
Reese Letts <rletts@unc.edu>
2024-11-14 19:05:21 -0500

    flask setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# main file
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(port=5000)  # Default port is 5000, but you can change this if you want
